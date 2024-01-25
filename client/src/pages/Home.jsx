ef3b3af6cd7adb765937060e273d420d70a5d08e
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2024-01-25 14:02:57 -0800

    Fixes to Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import { useQuery } from '@apollo/client';
 
-import ThoughtList from '../components/ThoughtList';
-import ThoughtForm from '../components/ThoughtForm';
+//import ThoughtList from '../components/ThoughtList';
+//import ThoughtForm from '../components/ThoughtForm';
 
-import { QUERY_THOUGHTS } from '../utils/queries';
+//import { QUERY_THOUGHTS } from '../utils/queries';
 
 const Home = () => {
-  const { loading, data } = useQuery(QUERY_THOUGHTS);
-  const thoughts = data?.thoughts || [];
+  //const { loading, data } = useQuery(QUERY_THOUGHTS);
+  //const thoughts = data?.thoughts || [];
 
   return (
     <main>
@@ -16,17 +16,7 @@ const Home = () => {
           className="col-12 col-md-10 mb-3 p-3"
           style={{ border: '1px dotted #1a1a1a' }}
         >
-          <ThoughtForm />
-        </div>
-        <div className="col-12 col-md-8 mb-3">
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <ThoughtList
-              thoughts={thoughts}
-              title="Some Feed for Thought(s)..."
-            />
-          )}
+          "Hello!"
         </div>
       </div>
     </main>
